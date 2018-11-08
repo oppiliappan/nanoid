121c7665a45fe40e44fbded8bb88a370e27e9dcc
NerdyPepper <akshayoppiliappan@gmail.com>
2018-11-08 11:19:34 +0530

    Minify to single header library

--- nanoid.h
+++ nanoid.h
@@ -2,8 +2,16 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
-#include "alphabet.h"
 
+// default alphabet list 
+// updated to nanoid 2.0
+static char alphs[] = {
+    '-', '_',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '\0'
+};
 // custom alphabet pool and length
 char* custom(char alphs[], int size);
 

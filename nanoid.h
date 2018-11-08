488d865b95194d057a2d0c8c2e93c1f7424b66ac
NerdyPepper <akshayoppiliappan@gmail.com>
2018-11-08 12:08:19 +0530

    Fix modulo bias

--- nanoid.h
+++ nanoid.h
@@ -21,13 +21,19 @@ char* generate(int size);
 // default character pool and 21 in length
 char* simple();
 
+
 char* custom(char alphs[], int size) {
-    srand(unsigned(time(0)));
+    srand(time(NULL));
     int alph_size = strlen(alphs) - 1;
     char *id = (char *) malloc(sizeof(char) * 3);
 
     for( int i = 0; i < size; i++ ) {
-        int random_num = rand() % (alph_size);
+        int random_num;
+        do {
+            random_num = rand();
+        } while (random_num >= (RAND_MAX - RAND_MAX % alph_size));
+
+        random_num %= alph_size;
         id[i] = alphs[random_num];
     }
 
